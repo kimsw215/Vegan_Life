b580c7789396a5eeac7a9bf4282681bd7883f1c6
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2022-06-26 20:56:34 +0900

    Initial commit splsh activity

---
+++ app/src/main/java/kr/ac/kpu/ce2019152012/vegan_life/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.ac.kpu.ce2019152012.vegan_life
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/kr/ac/kpu/ce2019152012/vegan_life/SplashActivity/SplashActivity.kt
@@ -0,0 +1,30 @@
+package kr.ac.kpu.ce2019152012.vegan_life.SplashActivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import kr.ac.kpu.ce2019152012.vegan_life.MainActivity
+import kr.ac.kpu.ce2019152012.vegan_life.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        }, Duration)
+    }
+
+    companion object {
+        private const val Duration: Long = 2000
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+}
